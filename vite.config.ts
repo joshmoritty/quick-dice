1375019d68482ad649919d01416d8939d69adc39
joshmoritty <josh.moritty@gmail.com>
2025-11-02 08:13:58 +0700

    feat: make pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo.svg'],
+      manifest: {
+        name: 'Quick Dice',
+        short_name: 'Quick Dice',
+        description: 'A quick, minimal dice roller for DnD.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/quick-dice/',
   resolve: {
     alias: {
